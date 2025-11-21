e7e4bdce3e668e2d2fec93fb0a684d8d2512d80e
Luiz8734 <luizmoraissantos17@gmail.com>
2025-11-21 19:51:26 -0300

    arrumando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ModalPerfil from './components/ModalPerfil';
 import ModalMensagem from './components/ModalMensagem';
 import BarraBusca from './components/BarraBusca';
 import Filtros from './components/Filtros ';
-import ToggleModoEscuro from './components/ToggleModoEscuro';
+import ToggleModoEscuro from './components/ToggleModoEscur';
 
 /**
  * Componente principal da aplicação
